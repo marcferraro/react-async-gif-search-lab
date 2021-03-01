63b4d194c8128d62f9e65ca1d603304a6dff1718
Marc <marc.ferraro1@gmail.com>
2021-02-28 23:01:48 -0600

    Inital gif rendering

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default class GifList extends React.Component{
     render(){
         return(
-            <div>Hello from gif list</div>
+            // <div>Hello from gif list</div>
+            <ul>
+                {this.props.gifs.map(gif => <li key={gif.id}><img src={gif.images.original.url} alt="A gif from Giphy"></img></li>)}
+            </ul>
+            
         )
     }
 }
--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -4,13 +4,36 @@ import GifList from '../components/GifList'
 
 
 export default class GifListContainer extends React.Component{
+    
+    constructor(){
+        super()
+        this.state = {
+            gifs: []
+        }
+    }
+
+    splashGifs =  () => {
+        fetch('https://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g')
+        .then(resp => resp.json())
+        .then(gifs => {
+            console.log(gifs['data'].slice(0, 3))
+            this.setState({
+                gifs: gifs['data'].slice(0, 3)
+            })
+        })
+    }
+
+
     render(){
         return(
             <div>
-                <div>Hello from gif list container</div>
                 < GifSearch/>
-                < GifList/>
+                < GifList gifs={this.state.gifs} />
             </div>
         )
+    }
+
+    componentDidMount(){
+        this.splashGifs()
     }
 }
